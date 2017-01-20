a59286605e33a07e815a4fe9ec4bd78db3258c70
Lyes Kriaa <LyesKriaa@Sansfil-Eduroam-Lassonde-220-33.polymtl.ca>
2017-01-19 19:46:03 -0500

    first commit

---
+++ TP1_INF8405/app/src/main/java/com/example/lyeskriaa/tp1_inf8405/MenuPrincipal.java
@@ -0,0 +1,43 @@
+package com.example.lyeskriaa.tp1_inf8405;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    private Button exit_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+
+        exit_btn = (Button) findViewById(R.id.button3);
+
+        exit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exit();
+            }
+        });
+    }
+
+    private void exit() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+
+}
+
