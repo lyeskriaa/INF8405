5bcca9b7c1ca4b5ff083808389542110fdc160ef
LyesKriaa <lyeskriaa@gmail.com>
2017-02-01 23:46:32 -0500

    igames pour la grid

--- TP1_INF8405/app/src/main/java/com/example/lyeskriaa/tp1_inf8405/ImageAdapter.java
+++ TP1_INF8405/app/src/main/java/com/example/lyeskriaa/tp1_inf8405/ImageAdapter.java
@@ -7,15 +7,19 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 /**
  * Created by LyesKriaa on 17-01-26.
  */
 
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
+    private Niveaux nivGrid;
 
-    public ImageAdapter(Context c) {
+    public ImageAdapter(Context c, Niveaux nGrid) {
         mContext = c;
+        nivGrid = nGrid;
     }
 
     public int getCount() {
@@ -43,22 +47,23 @@ public class ImageAdapter extends BaseAdapter {
             imageView = (ImageView) convertView;
         }
 
+        fillGrid(nivGrid.getGrilleNiveaux());
         imageView.setImageResource(mThumbIds[position]);
         return imageView;
     }
 
     // references to our images
-    private Integer[] mThumbIds = {
-            R.mipmap.level_1, R.mipmap.level_2,
-            R.mipmap.lock_level, R.mipmap.lock_level,
-            R.mipmap.lock_level, R.mipmap.lock_level,
-            R.mipmap.lock_level, R.mipmap.lock_level,
-            R.mipmap.lock_level, R.mipmap.lock_level,
-            R.mipmap.lock_level, R.mipmap.lock_level,
-            R.mipmap.lock_level, R.mipmap.lock_level,
-            R.mipmap.lock_level, R.mipmap.lock_level,
-            R.mipmap.lock_level, R.mipmap.lock_level,
-            R.mipmap.lock_level, R.mipmap.lock_level,
-            R.mipmap.lock_level, R.mipmap.lock_level
-    };
+    private Integer[] mThumbIds = new Integer[21];
+
+    private void fillGrid(ArrayList<Boolean> nivTab) {
+
+        mThumbIds[0] = (nivTab.get(0)) ? R.mipmap.level_1 : R.mipmap.lock_level;
+        mThumbIds[1] = (nivTab.get(1)) ? R.mipmap.level_2 : R.mipmap.lock_level;
+        mThumbIds[2] = (nivTab.get(2)) ? R.mipmap.level_3 : R.mipmap.lock_level;
+        mThumbIds[3] = (nivTab.get(3)) ? R.mipmap.level_4 : R.mipmap.lock_level;
+
+        for(int j=4; j<21; j++){
+            mThumbIds[j] = R.mipmap.lock_level;
+        }
+    }
 }
--- TP1_INF8405/app/src/main/java/com/example/lyeskriaa/tp1_inf8405/Niveaux.java
+++ TP1_INF8405/app/src/main/java/com/example/lyeskriaa/tp1_inf8405/Niveaux.java
@@ -7,22 +7,43 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class Niveaux extends AppCompatActivity {
+
+    public final static int NIV_1 = 1;
+    public final static int NIV_2 = 2;
+    public final static int NIV_3 = 3;
+    public final static int NIV_4 = 4;
+
+    private ArrayList<Boolean> grilleNiveaux = new ArrayList<Boolean>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_niveaux);
 
+        grilleNiveaux.add(true);
+        grilleNiveaux.add(false);
+        grilleNiveaux.add(false);
+        grilleNiveaux.add(false);
+
         GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
+        gridview.setAdapter(new ImageAdapter(this,this));
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(Niveaux.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(Niveaux.this, "" + position, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public ArrayList<Boolean> getGrilleNiveaux() {
+        return grilleNiveaux;
+    }
+
+    public void setGrilleNiveaux(int index, Boolean nivStatus) {
+        this.grilleNiveaux.set(index, nivStatus);
     }
 
 }
