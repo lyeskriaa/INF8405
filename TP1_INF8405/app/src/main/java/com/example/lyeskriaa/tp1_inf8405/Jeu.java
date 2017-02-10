5a07f8d2056fb1de3e18c6737334e7f1611d9149
LyesKriaa <lyeskriaa@gmail.com>
2017-02-10 14:22:13 -0500

    rectification des couleurs et ajour du touchListener

--- TP1_INF8405/app/src/main/java/com/example/lyeskriaa/tp1_inf8405/Jeu.java
+++ TP1_INF8405/app/src/main/java/com/example/lyeskriaa/tp1_inf8405/Jeu.java
@@ -8,21 +8,26 @@ import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
+
 
 public class Jeu extends AppCompatActivity {
-    @ColorInt public static final int PURPLE       = 0xFFFFA500;
-    @ColorInt public static final int ORANGE      = 0xFF800080;
+    @ColorInt public static final int PURPLE       = 0xFF660066;
+    @ColorInt public static final int ORANGE      = 0xFFFFAE00;
     @ColorInt public static final int GREEN      = 0xFF44DF06;
     @ColorInt public static final int YELLOW      = 0xFFF4F405;
 
-    Logique log;
-    GridLayout gridLayout;
-    GridLayout gridJeu;
+    public static Logique log;
+    private GridLayout gridLayout;
+    private GridLayout gridJeu;
+    private float x_start, x_end, y_start, y_end;
+    private static final int MIN_SWIPE = 140;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +38,7 @@ public class Jeu extends AppCompatActivity {
         gridLayout = (GridLayout) findViewById(R.id.gridLayoutJeu);
         dessinerGrilleJeu(log.getGrille(), gridLayout);
        // gridLayout.addView(gridJeu);
-        Toast.makeText(Jeu.this,log.getGrille().length + " / " + log.getGrille()[0].length, Toast.LENGTH_LONG).show();
+       // Toast.makeText(Jeu.this,log.getGrille().length + " / " + log.getGrille()[0].length, Toast.LENGTH_LONG).show();
     }
 
     private void dessinerGrilleJeu(int[][] grilleLogique, GridLayout gridLayout){
@@ -41,8 +46,8 @@ public class Jeu extends AppCompatActivity {
 
         gridLayout.removeAllViews();
 
-        int column = grilleLogique[0].length;
-        int row = grilleLogique.length;
+        final int column = grilleLogique[0].length;
+        final int row = grilleLogique.length;
         int total = column * row;
 
         gridLayout.setColumnCount(column);
@@ -54,10 +59,10 @@ public class Jeu extends AppCompatActivity {
                 c = 0;
                 r++;
             }
-            ImageView oImageView = new ImageView(this);
+            final ImageView oImageView = new ImageView(this);
             Bitmap tempBitmap = Bitmap.createBitmap(110,110,Bitmap.Config.ARGB_8888);
             tempBitmap = tempBitmap.copy(tempBitmap.getConfig(), true);
-            Canvas canvas = new Canvas(tempBitmap);
+            final Canvas canvas = new Canvas(tempBitmap);
             canvas.drawBitmap(tempBitmap,0,0,null);
 
             Paint paint = new Paint();
@@ -74,8 +79,55 @@ public class Jeu extends AppCompatActivity {
             param.setGravity(Gravity.CENTER);
             param.columnSpec = GridLayout.spec(c);
             param.rowSpec = GridLayout.spec(r);
+            oImageView.setId(i);
 
-            oImageView.setLayoutParams (param);
+            oImageView.setLayoutParams(param);
+            oImageView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    int action = MotionEventCompat.getActionMasked(event);
+
+                    switch(action) {
+                        case (MotionEvent.ACTION_DOWN) :
+                            x_start = event.getX();
+                            y_start = event.getY();
+                            return true;
+
+                        case (MotionEvent.ACTION_UP) :
+
+                            x_end = event.getX();
+                            y_end = event.getY();
+                            float deltaX = x_start - x_end;
+                            float deltaY = y_start - y_end;
+                            if ((Math.abs(deltaX) > MIN_SWIPE)  || (Math.abs(deltaY) > MIN_SWIPE))
+                            {
+                                int dir = 0;
+                                int x = oImageView.getId() / row ;
+                                int y = oImageView.getId() % row ;
+
+                                if(deltaX > 0 && (Math.abs(deltaX) > Math.abs(deltaY)*2))
+                                    dir = Logique.DROITE;
+                                if(deltaX < 0 && (Math.abs(deltaX) > Math.abs(deltaY)*2))
+                                    dir = Logique.GAUCHE;
+                                if(deltaY > 0 && (Math.abs(deltaY) > Math.abs(deltaX)*2))
+                                    dir = Logique.BAS;
+                                if(deltaY < 0 && (Math.abs(deltaY) > Math.abs(deltaX)*2))
+                                    dir = Logique.HAUT;
+
+                                log.bougerPiece(x,y,dir);
+                                //boolean animating = true;
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+
+                        default :
+                            return false;
+                    }
+                }
+            });
 
             gridLayout.addView(oImageView);
 
@@ -94,7 +146,7 @@ public class Jeu extends AppCompatActivity {
             case Logique.VERT:
                 couleur = GREEN;
                 break;
-            case Logique.N_ORANGE:
+            case Logique.ORANGE:
                 couleur = ORANGE;
                 break;
             case Logique.JAUNE:
@@ -103,10 +155,14 @@ public class Jeu extends AppCompatActivity {
             case Logique.VIOLET:
                 couleur = PURPLE;
                 break;
+            default:
+                couleur = Color.TRANSPARENT;
+                break;
         }
 
 
         return couleur;
 
     }
+
 }
