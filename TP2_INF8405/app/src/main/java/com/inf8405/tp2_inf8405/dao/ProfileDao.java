d8b68ec2bb05eade7f8003c35965c43cb51e271c
Louise Piotte <louise1p@hotmail.com>
2017-03-07 15:23:03 -0500

    group dao

--- TP2_INF8405/app/src/main/java/com/inf8405/tp2_inf8405/dao/ProfileDao.java
+++ TP2_INF8405/app/src/main/java/com/inf8405/tp2_inf8405/dao/ProfileDao.java
@@ -3,14 +3,15 @@ package com.inf8405.tp2_inf8405.dao;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.google.firebase.provider.FirebaseInitProvider;
+
 /**
  * Created by LyesKriaa on 17-03-07.
  */
 
 public class ProfileDao {
 
-    private SQLiteDatabase db;
-//    private MeetingDBHelper meetingdbHelper;
+    private FirebaseDatabase db;
     private Context context;
 
 
