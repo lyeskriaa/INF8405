674b40ac69b890d38602cbc665473b8429455f0b
Louise Piotte <louise1p@hotmail.com>
2017-03-19 20:04:02 -0400

    map camera move

--- TP2_INF8405/app/src/main/java/com/inf8405/tp2_inf8405/activities/MapsActivity.java
+++ TP2_INF8405/app/src/main/java/com/inf8405/tp2_inf8405/activities/MapsActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -69,6 +70,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
         refresh();
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(45.5,-73.6)));
 
     }
 
