b73874aaf75e21c3c9a1651a7b5b911e90b7a561
Louise Piotte <louise1p@hotmail.com>
2017-03-19 20:56:26 -0400

    newloc interface

--- TP2_INF8405/app/src/main/java/com/inf8405/tp2_inf8405/activities/MapsActivity.java
+++ TP2_INF8405/app/src/main/java/com/inf8405/tp2_inf8405/activities/MapsActivity.java
@@ -1,12 +1,12 @@
 package com.inf8405.tp2_inf8405.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -65,12 +65,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-                mMap.addMarker(new MarkerOptions().position(latLng).title("marker here"));
-                //TODO add input name and photo
+                Intent intent = new Intent(MapsActivity.this, NewLocationActivity.class);
+                Bundle b = new Bundle();
+                b.putDouble("longitude", latLng.longitude);
+                b.putDouble("latitude", latLng.latitude);
+                intent.putExtras(b); //Put your id to your next Intent
+                startActivity(intent);
             }
         });
         refresh();
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(45.5,-73.6)));
 
     }
 
@@ -117,11 +120,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         marker.snippet("location::"+event.getDateStart()+"::"+event.getDateEnd()+":image:"+event.getPicture());
         marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
         mMap.addMarker(marker);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        refresh();
     }
 }
---
+++ TP2_INF8405/app/src/main/java/com/inf8405/tp2_inf8405/activities/NewLocationActivity.java
@@ -0,0 +1,97 @@
+package com.inf8405.tp2_inf8405.activities;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.inf8405.tp2_inf8405.R;
+import com.inf8405.tp2_inf8405.model.Coordinate;
+import com.inf8405.tp2_inf8405.model.Group;
+import com.inf8405.tp2_inf8405.model.Lieu;
+
+import java.io.ByteArrayOutputStream;
+
+public class NewLocationActivity extends AppCompatActivity {
+
+    private Bitmap capturedImage;
+    private static final int REQUEST_IMAGE_CAPTURE = 111;
+    private double longitude;
+    private double latitude;
+    private String name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_location);
+        Bundle b = getIntent().getExtras();
+        longitude = -1; // or other values
+        if(b != null) longitude = b.getInt("longitude");
+        latitude = -1; // or other values
+        if(b != null) latitude = b.getInt("latitude");
+
+        final Button okButton = (Button) findViewById(R.id.newloc_okButton);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText nomLieu = (EditText) findViewById(R.id.newloc_NameEditText);
+                name = nomLieu.getText().toString();
+                if (capturedImage == null || name == null) {
+                    AlertDialog dialog = new AlertDialog.Builder(NewLocationActivity.this)
+                            .setTitle("Erreur")
+                            .setMessage("Il faut un nom de lieu et une photo!")
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                } else {
+                    Group.getGroup().addLoc(new Lieu(new Coordinate(longitude, latitude), name, encodeBitmap(capturedImage),0));
+                    finish();
+                }
+            }
+        });
+
+        final Button photoButton = (Button) findViewById(R.id.newloc_pictureButton);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(NewLocationActivity.this.getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+
+
+
+    //TODO adjust
+    // mettre la photo dans le cadre specifie et l encoder pour la sauvegarder sur firebase
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == this.RESULT_OK) {
+            Bundle extras = data.getExtras();
+            capturedImage = (Bitmap) extras.get("data");
+            ImageView mImageLabel = (ImageView) findViewById(R.id.newloc_photoIamgeView);
+            mImageLabel.setImageBitmap(capturedImage);
+        }
+    }
+
+
+    // copresser l'image en PNG et l'encoder en string
+    private String encodeBitmap(Bitmap imageBitmap) {
+        if (imageBitmap != null) {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            return Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+        } else {
+            return null;
+        }
+    }
+
+}
