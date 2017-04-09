8c4cd04682f23f62c728afa3202fbf03e33bf00e
Louise Piotte <louise1p@hotmail.com>
2017-04-09 13:52:22 -0400

    chat interface

---
+++ projet_INF8405/app/src/main/java/com/inf8405/projet_inf8405/activities/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.inf8405.projet_inf8405.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.inf8405.projet_inf8405.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
